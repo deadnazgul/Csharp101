4ef057348c6a05c17a02767d676f9d9c192e1f52
deadnazgul <atakan0859@gmail.com>
2022-04-29 15:12:38 +0300

    c#tipdönüşümleri
    c#101 tip dönüşümleri dersi

---
+++ PatikaDev- Pratikler/tip-Dönüşümleri/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+//.Net 5 için hali hazırda aşağıdaki kod satırları otomatik olarak sunuluyor ama .Net 6 da bunlar yok
+/*
+using System;
+
+namespace tip-Dönüşümleri{
+    class Program{
+        static void Main(string[] args){
+
+             
+
+        }
+    }
+}
+*/
+
+
+Console.WriteLine("Hello, World!");
+
+// Implicit Conversion
+
+byte a = 5;
+sbyte b = 30;
+short c = 10;                                  
+
+int d = a + b + c;
+Console.WriteLine("d: "+d);
+
+long h = d;
+Console.WriteLine("hi: "+h);
+
+float i = h;
+Console.WriteLine("i: "+i);
+
+string e = "deadnazgul";
+char f = 'K';
+object g = e + f + d;
+Console.WriteLine("g: "+ g);
+
+// Explicit Conversion
+
+Console.WriteLine("************ Explicit Conversion ***********");
+int x = 4;
+byte y = (byte)x;
+Console.WriteLine("y: "+y);
+
+int z = 100;
+byte t = (byte)z;
+Console.WriteLine("t: "+t);
+
+float w = 10.3f;
+byte v = (byte)w;
+Console.WriteLine("v: "+v);
+
+// *** ToString Methodu ***
+
+Console.WriteLine("****** ToString Methodu ******");
+int xx = 6;
+string yy = xx.ToString();
+Console.WriteLine("yy: "+yy);
+
+string zz = 12.5f.ToString();
+Console.WriteLine("zz: "+zz);
+
+// System.Convert
+Console.WriteLine("****** System.Convert sınıfı ******");
+string s1 = "10", s2 = "20";
+int sayı1, sayı2;
+int Toplam;
+
+sayı1 = Convert.ToInt32(s1);
+sayı2 = Convert.ToInt32(s2);
+Toplam = sayı1 + sayı2;
+Console.WriteLine("Toplam: "+Toplam);
+
+// Parse
+Console.WriteLine("****** Parse Methodu ******");
+ParseMethod();
+
+static void ParseMethod(){
+    string metin1 = "10";
+    string metin2 = "10.25";
+    int rakam1;
+    double double1;
+    
+    rakam1 = Int32.Parse(metin1); // parse herzaman string Alır
+    double1 = Double.Parse(metin2);
+
+    Console.WriteLine("rakam1: "+rakam1);
+    Console.WriteLine("double1: "+double1);
+}
+
+
