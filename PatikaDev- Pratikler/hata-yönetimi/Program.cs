966723df6930e6baea3e444d01527437244a17de
deadnazgul <atakan0859@gmail.com>
2022-04-30 08:50:45 +0300

    C#101_hata_yönetimi
    C#101 error exception handling

---
+++ PatikaDev- Pratikler/hata-yönetimi/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("bir sayı giriniz: ");
+
+try{
+   int sayı;
+   sayı = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("Girmiş olduğunuz sayı: "+sayı);
+}
+catch(Exception ex){
+  Console.WriteLine("Hata: "+ex.Message.ToString());
+}
+finally{ // optional
+   Console.WriteLine("işlem tamamlandı.");
+}
+
+try{
+   int a  = int.Parse(null);
+}
+catch(ArgumentNullException ex){
+   Console.WriteLine("Boş değer girdiniz.");
+   Console.WriteLine(ex);
+}
+try{
+    int b  = int.Parse("test");
+}
+catch(FormatException ex){
+    Console.WriteLine("Veri tip uygun değil.");
+    Console.WriteLine(ex);
+}
+try{
+    int c  = int.Parse("-20000000000");
+}
+catch(OverflowException ex){
+    Console.WriteLine("Çok küçük yada çok büyük bir sayı girdiniz.");
+    Console.WriteLine(ex);
+}
+finally{
+    Console.WriteLine("İşlem başarı ile tamamlandı");
+}
