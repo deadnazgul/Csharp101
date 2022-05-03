c172eb5b33f3db38e3a3ff61753653c339ad868e
deadnazgul <atakan0859@gmail.com>
2022-05-04 01:41:40 +0300

    C#101WhileForeach
    foreach ve while döngüleri yapısı ve örneği patika dev

---
+++ PatikaDev- Pratikler/döngüler-WhileForEach/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// While
+// 1 den başlayarak konsolda girilen sayıya kadar 
+// ortalama hesaplayan program
+
+Console.WriteLine("Bir sayı giriniz: ");
+int sayı = int.Parse(Console.ReadLine());
+int toplam = 0;
+int sayaç = 1;
+while(sayaç <= sayı){
+    toplam += sayaç;
+    sayaç++;
+}
+Console.WriteLine("ortalama: "+toplam/sayı);
+
+// 'a' dan 'z' ye kadar tüm harfleri console a yazdır.
+char character = 'a';
+while(character < 'z'){
+    Console.Write(character+"-");
+    character++;
+}
+Console.WriteLine();
+
+Console.WriteLine("********** Foreach **********");
+string[] arabalar = {"BMW","Ford","Wolkswagen","Audi"};
+foreach (var item in arabalar)
+{
+    Console.WriteLine(item);
+}
