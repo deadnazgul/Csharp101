e18af2ba76000fe4f4bda0df07ea172a2619a300
deadnazgul <atakan0859@gmail.com>
2022-05-08 22:17:31 +0300

    C#101 metotOverloading
    Nasıl metot overloading yapılır şartlar ve metot imzası nedir out kullanımı

---
+++ PatikaDev- Pratikler/metotlar-overloading/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// out parametreler
+string sayi = "999";
+
+bool sonuc = int.TryParse(sayi, out int outSayi);
+if(sonuc){
+    Console.WriteLine("Başarılı!");
+    Console.WriteLine(outSayi);
+}else{
+    Console.WriteLine("Başarısız...!");
+}
+
+Metotlar instance = new Metotlar();
+instance.Topla(4,5, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+
+// Metot Aşırı Yükleme - Overloading
+int ifade = 333;
+instance.EkranaYazdır(Convert.ToString(ifade+" Tostring ile yazıldı."));
+instance.EkranaYazdır(ifade + " Metot overloading ile yazıldı.");
+
+// metot imzası
+// metotAdı + parametre sayısı + parametre
+
+instance.EkranaYazdır("Atakan ","Furkan");
+
+class Metotlar{
+    public void Topla(int a, int b, out int toplam){
+        toplam = a + b;
+    }
+    public void EkranaYazdır(string veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdır(int veri){
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdır(String veri1, string veri2){
+        Console.WriteLine(veri1 + veri2);
+    }
+}
