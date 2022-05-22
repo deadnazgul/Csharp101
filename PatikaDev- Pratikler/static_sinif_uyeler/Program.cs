f990a4c50498a0509be3367673a9f6c93de2b660
deadnazgul <atakan0859@gmail.com>
2022-05-22 11:20:40 +0300

    C#101_Static_Sınıf_uyeler
    C#101 static Class ve static constructor kullanımı

---
+++ PatikaDev- Pratikler/static_sinif_uyeler/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Çalışan Sayısı: {0}",Calisan.CalisanSayisi);
+
+Calisan calisan_1 = new Calisan("atakan","uludağ","Software Developer");
+Console.WriteLine("Çalışan Sayısı: {0}",Calisan.CalisanSayisi);
+calisan_1.EkranaYazdir();
+
+//Islemler islemler_1 = new Islemler(); --> static class lar bu şekilde olmaz
+// direk ismiyle erişcen...
+long toplama = Islemler.Topla(100,200);
+long cikarma = Islemler.Cikar(400,50);
+Console.WriteLine("Toplama işlemi : --> {0}",toplama);
+Console.WriteLine("Çıkarma işlemi : --> {0}",cikarma);
+
+
+
+class Calisan{
+    public static int calisanSayisi;
+    public static int CalisanSayisi{
+        get{return calisanSayisi;}
+        
+    }
+    private string Isim;
+    private string Soyisim;
+    private string Departman;
+    static Calisan(){
+        calisanSayisi = 0;
+    }
+    public Calisan(string isim,string soyisim,string departman){
+        this.Isim = isim;
+        this.Soyisim = soyisim;
+        this.Departman = departman;
+        calisanSayisi++;
+    }
+    public void EkranaYazdir(){
+        Console.WriteLine("İsim : {0}",Isim);
+        Console.WriteLine("Soyİsim : {0}",Soyisim);
+        Console.WriteLine("Departman: {0}",Departman);
+    }
+}
+
+static class Islemler{
+    public static long Topla(int sayi1,int sayi2){
+        return sayi1 + sayi2;
+    }
+    public static long Cikar(int sayi1,int sayi2){
+        return sayi1 - sayi2;
+    }
+}
