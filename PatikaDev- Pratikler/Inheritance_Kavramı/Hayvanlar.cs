657f90934594a37630fdd1e52cf45ece1d215660
deadnazgul <atakan0859@gmail.com>
2022-05-25 17:37:41 +0300

    C#101_Inheritance_Kavramı
    Canlılar-->Hayvanlar=>Sürüngenler=>Kuşlar,-->Bitkiler=>Tohumlu0>Tohumsuz,
    örnekler ile inheritance anlatımı#######################

---
+++ PatikaDev- Pratikler/Inheritance_Kavramı/Hayvanlar.cs
@@ -0,0 +1,29 @@
+public class Hayvanlar:Canlilar{
+    protected void Adaptasyon(){
+        Console.WriteLine("Hayvanlar adaptasyon kurabilir.");
+    }
+}
+
+public class Surungenler:Hayvanlar{
+    public Surungenler(){
+        base.Adaptasyon();
+        base.Beslenme();
+        base.Bosaltim();
+        base.Solunum();
+    }
+    public void SurunerekHareketEderler(){
+        Console.WriteLine("Sürüngenler sürünerek hareket ederler.");
+    }
+}
+
+public class Kuşlar:Hayvanlar{
+    public Kuşlar(){
+        base.Adaptasyon();
+        base.Beslenme();
+        base.Bosaltim();
+        base.Solunum();
+    }
+    public void Ucmak(){
+        Console.WriteLine("Kuşlar uçar.");
+    }
+}
---
+++ PatikaDev- Pratikler/Inheritance_Kavramı/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+-------------- Canlılar --------------
+--> Bitkiler                --> Hayvanlar
+1-) Tohumlu                 1-) Sürüngenler
+2-) Tohumsuz                2-) Kuşlar
+
+*/
+
+TohumluBitkiler tohumlubitki_1 = new TohumluBitkiler();
+// tohumlubitki_1.Beslenme();
+// tohumlubitki_1.Solunum();
+// tohumlubitki_1.Bosaltim();
+//tohumlubitki_1.FotosentezYapmak();
+tohumlubitki_1.TohumlaCogalma();
+
+Console.WriteLine("* * * * * * * * * * * * * * * * * *");
+
+Kuşlar martı = new Kuşlar();
+// martı.Solunum();
+// martı.Beslenme();
+// martı.Bosaltim();
+//martı.Adaptasyon();
+martı.Ucmak();
